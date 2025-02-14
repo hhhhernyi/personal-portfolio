f302a3f0f5e27b20a6298083f2ed5b961d701d0b
Hern Yi Lee <hyileenet@gmail.com>
2025-02-14 23:20:16 +0800

    complete hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 // src/App.jsx
 
+import HeroGradient from "./components/navbar/heroSection/HeroGradient";
+import HeroMain from "./components/navbar/heroSection/HeroMain";
+import { SubHeroSection } from "./components/navbar/heroSection/SubHeroSection";
 import NavBarMain from "./components/navbar/NavBarMain";
 
 const App = () => {
   return(
     <main className="font-body">
       <NavBarMain />
+      <HeroMain />
+      <HeroGradient />
+      <SubHeroSection />
     </main>
   );
 };
---
+++ src/components/navbar/heroSection/HeroMain.jsx
@@ -0,0 +1,19 @@
+
+import HeroText from './HeroText'
+import HeroPic from './HeroPic'
+
+export default function HeroMain() {
+  return (
+    <div className='pt-40 pb-16'>
+        <div className='flex md:flex-row sm:flex-col max-w-[1200px] mx-auto justify-between items-center relative px-4 '> 
+            <HeroText />
+        <HeroPic />
+        </div>
+        
+
+    </div>
+  )
+}
+
+
+
